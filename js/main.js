667df2c63ac0a71ff6fd41abb1fb66c0ac339445
Adrian Bringas <jraul6711@gmail.com>
2026-01-29 02:45:56 -0600

    Step 2 completed

--- js/main.js
+++ js/main.js
@@ -1,25 +1,66 @@
 
-//obtencion de botones
+//Variables | Constantes
 const btnAnterior = document.getElementById("btnSiguiente");
 const btnSiguiente = document.getElementById("btnSiguiente");
 const textoPregunta = document.getElementById("textoPregunta");
 const contador = document.getElementById("contador");
+let respuestasUsuario = JSON.parse(localStorage.getItem("respuestas")) || [];
+const contenedorRespuestas = document.getElementById("respuestas");
+
+//contador
+let indiceActual = 0;
 
 //funciones
 function irAnterior() {
     console.log("Boton anterior presionado")
-    //logica para ir a la pregunta anterior
+    if (0 < indiceActual) {
+        indiceActual--;
+        mostrarPregunta();
+    }//if indice > 0
 }//irAnterior
 
 function irSiguiente() {
-    console.log("Boton siguiente presionado")
-    //logica para ir a la pregunta siguiente
+    console.log("Boton siguiente presionado");
+    if (indiceActual < preguntas.length) { //if (indiceActual < preguntas.length - 1) {
+        indiceActual++;
+        mostrarPregunta();
+    }//if indice < total de preguntas
 }//irSiguiente
 
 function mostrarPregunta() {
     const preguntaActual = preguntas[indiceActual];
     textoPregunta.textContent = preguntaActual.pregunta; //muestra texto de la pregunta actual
-    contador.textContent = (indiceActual + 1)
+    contador.textContent = (indiceActual + 1);
+        // Limpiar respuestas anteriores
+    contenedorRespuestas.innerHTML = "";
+
+    preguntaActual.opciones.forEach((opcion, index) => {
+        const label = document.createElement("label");
+        const input = document.createElement("input");
+
+        input.type = "radio";
+        input.name = "respuesta";
+        input.value = opcion;
+
+        // Si ya fue respondida, marcarla
+        if (respuestasUsuario[indiceActual] === opcion) {
+            input.checked = true;
+        }
+
+        // Guardar respuesta al hacer click
+        input.addEventListener("change", () => {
+            respuestasUsuario[indiceActual] = opcion;
+            localStorage.setItem(
+                "respuestas",
+                JSON.stringify(respuestasUsuario)
+            );
+        });
+
+        label.appendChild(input);
+        label.append(" " + opcion);
+        contenedorRespuestas.appendChild(label);
+        contenedorRespuestas.appendChild(document.createElement("br"));
+    });
 }//mostrarPregunta
 
 //eventos
@@ -43,5 +84,3 @@ const preguntas = [
     }
 ];
 
-//contador
-let indiceActual = 0;
